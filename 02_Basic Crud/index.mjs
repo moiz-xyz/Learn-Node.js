11cccfbf393f607ab0a8c50a778cae86cd03ec9b
moiz-xyz <a.moizniaxi123@gmail.com>
2025-05-28 14:47:50 +0500

    added crud and learned postman

--- 02_Basic Crud/index.mjs
+++ 02_Basic Crud/index.mjs
@@ -2,6 +2,8 @@
 import express from "express"
 
 const app = express ();
+app.use(express.json());
+
 const PORT = 3000; 
 
 const users = [
@@ -31,11 +33,27 @@ app.get("/users", (req , res)=>{
 res.send(users)
 })
 
+//  post users form postman
 app.post('/', (req ,res)=>{
 users.push({id : users.length +1 , ...req.body})
 res.send ({message :"user added succsfully"})
 })
 
+// delete function
+app.delete("/users/:id" , ( req, res)=>{
+    const getUserId = parseInt(req.params.id)
+    const index = users.findIndex(users => users.id === getUserId)
+    users.splice(index , 1)
+   res.send({ message: "User deleted successfully" });
+}) 
+
+// update function
+app.put("/users/:id" , (req , res)=>{
+    const getId = parseInt(req.params.id)
+    const index = users.findIndex(users => users.id === getId);
+     users.splice(index, 1, {id: Number(req.params.id), ...req.body})
+    res.send({ message: "User Update Successfully" })
+})
 
 app.listen(PORT ,()=>[
     console.log(`Server is running on http://localhost:${PORT}`)  
